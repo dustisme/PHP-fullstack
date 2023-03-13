4191d0f534be86e6568ef56776e212d3537c8871
dustisme <androsrivas@outlook.com>
2023-03-13 12:53:07 +0100

    exercici 1 nivell 2 tema 3-1

---
+++ Sprint 1/Tema 3-1/Nivell 2/ex1.php
@@ -0,0 +1,22 @@
+
+
+<html>
+<head>
+    <title>Exercici 1</title>
+</head>
+<body>
+    <?php
+        $n1 = 1;
+        $n2 = 2;
+
+        if ($n1==$n2) {
+            echo ($n1 + $n2)*2;
+            echo "<br>";
+        } else {
+            echo $n1 + $n2;
+            echo "<br>";
+        }
+    ?>
+</body>
+
+</html>
