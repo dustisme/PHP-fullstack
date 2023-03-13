d4fa51122f27ba3898c151ff21a59decf0827965
dustisme <androsrivas@outlook.com>
2023-03-13 13:06:31 +0100

    estructura feta

---
+++ Sprint 1/Tema 3-1/Nivell 2/ex2.php
@@ -0,0 +1,23 @@
+<!-- Escriu un programa PHP per intercanviar el primer i últim caràcter d'una cadena donada i imprimeix la nova cadena.
+
+Exemple
+Entrada
+wxyz
+a
+ab
+
+Sortida
+zxyw
+a
+ba  -->
+
+<html>
+<head>
+    <title>Execici 2</title>
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
