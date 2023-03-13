e9df97db515d0df34b0b3c2a967f1e3f2a4271ed
dustisme <androsrivas@outlook.com>
2023-03-13 12:59:57 +0100

    moved

---
+++ Sprint 1/Tema 3-1/Nivell 2/ex1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Exercici 1</title>
+</head>
+<body>
+    <?php
+        $n1 = 2;
+        $n2 = 2;
+
+        if ($n1==$n2) {
+            echo ($n1 + $n2)*2;
+            echo "<br>";
+        } else {
+            echo $n1 + $n2;
+            echo "<br>";
+        }
+    ?>
+</body>
+
+</html>
