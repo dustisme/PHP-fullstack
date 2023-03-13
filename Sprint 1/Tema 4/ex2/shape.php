6ebafb108be0438a5ed1f42f52887c1f2a972fb2
dustisme <androsrivas@outlook.com>
2023-03-13 12:15:53 +0100

    exercici fet

--- Sprint 1/Tema 4/ex2/shape.php
+++ Sprint 1/Tema 4/ex2/shape.php
@@ -5,24 +5,63 @@ que calculin respectivament l'àrea de la forma area().
 
 A l'arxiu main defineix dos objectes, un triangle i un 
 rectangle i truca al mètode area de cadascun. -->
+<html>
 
-<?php
-class Shape
-{
-    //atributs
-    public $height;
-    public $base;
+<head>
+    <title>Exercici 2</title>
+</head>
+<body>
 
 
-    //mètode constructor
-    public function __construct($alçada, $base)
+    <?php
+    class Shape
     {
-        $this->height = $alçada;
-        $this->base = $base;
+        //atributs
+        public $height;
+        public $base;
+
+
+        //mètode constructor
+        public function __construct($alçada, $base)
+        {
+            $this->height = $alçada;
+            $this->base = $base;
+        }
+
+        public function print($area)
+        {
+            echo "L'àrea és de: $area <br>";
+        }
     }
 
-    public function print($area) {
-        echo "L'àrea és de: $area";
+    class Triangle extends Shape
+    {
+        //mètode classe
+        public function area()
+        {
+            $area = $this->height * $this->base / 2;
+            return $area;
+        }
     }
-}
-?>
+
+    class Rectangle extends Shape
+    {
+        //mètode classe
+        public function area()
+        {
+            $area = $this->height * $this->base;
+            return $area;
+        }
+    }
+
+    class main
+    {
+    }
+    $triangle = new Triangle(45, 76);
+    echo print($triangle->area());
+    echo "<br>";
+    $rectangle = new Rectangle(56, 13);
+    echo print($rectangle->area());
+    ?>
+</body>
+</html>
