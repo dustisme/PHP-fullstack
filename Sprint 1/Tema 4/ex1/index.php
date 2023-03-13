7465313d0340570a643efc5b48967f2c30305caf
dustisme <androsrivas@outlook.com>
2023-03-13 11:04:42 +0100

    instantiate class

---
+++ Sprint 1/Tema 4/ex1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $emp1 = new Employee();
+    $emp1->initialize("Joel", 1700);
+    $emp1->print();
+
+    $emp2 = new Employee();
+    $emp2->initialize("Ellie", 2500);
+    $emp2->print();
+?>
