73f86af90a166a40b463aa9d96d5e186240fa1eb
dustisme <androsrivas@outlook.com>
2023-03-13 12:20:12 +0100

    modified

--- Sprint 1/Tema 4/ex1.php
+++ Sprint 1/Tema 4/ex1.php
@@ -1,6 +1,3 @@
-<!-- Crea una classe Employee, defineix com a atributs el seu nom i sou. Definir un mètode initialize que rebi com a
-paràmetres el nom i sou. Plantejar un segon mètode print que imprimeixi el nom i un missatge si ha de pagar o no
-impostos (si el sou supera 6000, paga impostos). -->
 <html>
 
 <head>
--- Sprint 1/Tema 4/ex2.php
+++ Sprint 1/Tema 4/ex2.php
@@ -1,10 +1,3 @@
-<!-- Escriu un programa que defineixi una classe Shape amb un
-constructor que rebi com a paràmetres l'ample i alt. Defineix
-dues subclasses; Triangle i Rectangle que heretin de Shape i 
-que calculin respectivament l'àrea de la forma area().
-
-A l'arxiu main defineix dos objectes, un triangle i un 
-rectangle i truca al mètode area de cadascun. -->
 <html>
 
 <head>
